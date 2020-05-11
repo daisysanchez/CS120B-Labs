3e541a6aedb35991ea40160ae14e8a657664ff86
Daisy Sanchez <sanchez.daisy86@gmail.com>
2020-05-10 23:15:59 -0700

    ex 1

--- lab7_lcd/source/main.c
+++ lab7_lcd/source/main.c
@@ -8,15 +8,114 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include "io.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum States{start, release, press, hold, reset} state;
+
+unsigned char button0 = 0x00;
+unsigned char button1 = 0x00;
+
+unsigned char cnt = 0;
+
+void Tick(){
+	switch(state){
+		case start:
+			state = release;
+			break;
+
+		case release:
+			if(button0 ^ button1){
+				state = press;
+			} else if (button0 && button1){
+				state = reset;
+			} else {
+				state = release;
+			}
+			break;
+		case press:
+			if(button0 ^ button1){
+				state = hold;
+			} else if(button0 && button1){
+				state = reset;
+			} else {
+				state = release;
+			}
+			break;
+		case hold:
+			if(button0 ^ button1){
+				state = hold;
+			} else if (button0 && button1){
+				state = reset;
+			} else {
+				state = release;
+			} 
+			break;
+		case reset:
+			if(button0 && !button1){
+				state = release;
+			} else {
+				state = reset;
+			} 
+			break;
+		default:
+			break;
+	}
+
+	switch(state){
+		case press:
+			if(button0 && cnt<9){
+				cnt++;
+				LCD_ClearScreen();
+				LCD_WriteData (cnt + '0');
+			} else if(button1 && cnt > 0){
+				cnt --;
+				LCD_ClearScreen();
+				LCD_WriteData(cnt + '0');
+			} else {
+			}
+			break;
+		case hold:
+			break;
+		case reset:
+			cnt = 0;
+			LCD_ClearScreen();
+			LCD_WriteData('0');
+			break;
+		case release:
+			break;
+		default:
+			break;
+	}
+}
+	
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+
+	state = start;
+
+	TimerSet(1000);
+	TimerOn();
+
+	LCD_init();
+
+//	LCD_DisplayString(1, "Hello World");
 
     /* Insert your solution below */
     while (1) {
+//	    LCD_DisplayString(1, "Hello World");
+	    button0 = ~PINA & 0x01;
+	    button1 = ~PINA & 0x02;
+	    Tick();
+	    while(!TimerFlag);
+	    TimerFlag=0;
+	    continue;
 
     }
     return 1;
