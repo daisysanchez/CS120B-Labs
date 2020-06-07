64fafb2cb5892e5ad848f91cd5286abfab161dd7
Daisy Sanchez <sanchez.daisy86@gmail.com>
2020-06-07 16:32:44 -0700

    retreiving keypad.h

--- lab11_LCDRacer/source/main.c
+++ lab11_LCDRacer/source/main.c
@@ -8,46 +8,48 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <../header/bit.h>
+#include <../header/keypad.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-unsigned char GetKeypadKey() {
+/*unsigned char GetKeypadKey() {
 	//checks keys in col 1
 	PORTC = 0xEF; //enable col 4 width 0, disable others with 1's
 	asm("nop"); //add a delay to allow PORTC to stablizie before checking
-	if(GetBit(PINC, 0) == 0) {return '1';}
-	if(GetBit(PINC, 1) == 0) {return '4';}
-	if(GetBit(PINC, 2) == 0) {return '7';}
-	if(GetBit(PINC, 3) == 0) {return '*';}
+	if(GetBit(~PINC, 0) == 0) {return('1');}
+	if(GetBit(~PINC, 1) == 0) {return('4');}
+	if(GetBit(~PINC, 2) == 0) {return('7');}
+	if(GetBit(~PINC, 3) == 0) {return('*');}
 
 	//check keys in col 2
 	PORTC = 0xDF; //enable col 5 with 0, disable others with 1's
 	asm("nop"); //add a delay to allow PORTC to stabilize before checking
 	if(GetBit(PINC, 0) == 0) {return('2');}	
-	if(GetBit(PINC, 1) == 0) {return '5';}
-	if(GetBit(PINC, 3) == 0) {return '8';}
-	if(GetBit(PINC, 4) == 0) {return '0';}
+	if(GetBit(PINC, 1) == 0) {return('5');}
+	if(GetBit(PINC, 3) == 0) {return('8');}
+	if(GetBit(PINC, 4) == 0) {return('0');}
 	
 	//checks keys in col 3
 	PORTC = 0xBF; //enable col 6 with 0, disables other's with 1's
 	asm("nop");
-	if(GetBit(PINC, 0) == 0) {return '3';}
-	if(GetBit(PINC, 1) == 0) {return '6';}
-	if(GetBit(PINC, 2) == 0) {return '9';}
-	if(GetBit(PINC, 3) == 0) {return '#';}
+	if(GetBit(PINC, 0) == 0) {return('3');}
+	if(GetBit(PINC, 1) == 0) {return('6');}
+	if(GetBit(PINC, 2) == 0) {return('9');}
+	if(GetBit(PINC, 3) == 0) {return('#');}
 	
 	//checks keys in col 4
-	PORTC = 8F;
+	PORTC = 0x8F;
 	asm("nop");
-	if(GetBit(PINC, 0) == 0) {return 'A';}
-	if(GetBit(PINC, 1) == 0) {return 'B';}
-	if(GetBit(PINC, 2) == 0) {return 'C';}
-	if(GetBit(PINC, 3) == 0) {return 'D';}
+	if(GetBit(PINC, 0) == 0) {return('A');}
+	if(GetBit(PINC, 1) == 0) {return('B');}
+	if(GetBit(PINC, 2) == 0) {return('C');}
+	if(GetBit(PINC, 3) == 0) {return('D');}
 
 	return('\0'); //default value
 
-}
+}*/
 
 
 int main(void) {
@@ -77,8 +79,8 @@ int main(void) {
 		case 'B': PORTB = 0x0B; break;
 		case 'C': PORTB = 0x0C; break;
 		case 'D': PORTB = 0x0D; break;
-		case '*': PORTB = 0x1E; break;
-		case '#': PORTB = 0x1F; break;
+		case '*': PORTB = 0x0E; break;
+		case '#': PORTB = 0x0F; break;
 		default: PORTB = 0x1B; break; //should nevewr occur, middle led off
 	}
     }
