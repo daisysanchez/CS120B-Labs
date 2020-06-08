6b3ead0e14207f929ec4f6a6472de359880ea562
Daisy Sanchez <sanchez.daisy86@gmail.com>
2020-06-07 20:45:02 -0700

    retrieving timer.h

--- lab11_LCDRacer/source/main.c
+++ lab11_LCDRacer/source/main.c
@@ -10,47 +10,49 @@
 #include <avr/io.h>
 //#include <../header/bit.h>
 #include <../header/keypad.h>
+#include <../header/timer.h>
+#include <../header/scheduler.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-/*unsigned char GetKeypadKey() {
-	//checks keys in col 1
-	PORTC = 0xEF; //enable col 4 width 0, disable others with 1's
-	asm("nop"); //add a delay to allow PORTC to stablizie before checking
-	if(GetBit(~PINC, 0) == 0) {return('1');}
-	if(GetBit(~PINC, 1) == 0) {return('4');}
-	if(GetBit(~PINC, 2) == 0) {return('7');}
-	if(GetBit(~PINC, 3) == 0) {return('*');}
+enum keypadStates{start} keypadStates;
 
-	//check keys in col 2
-	PORTC = 0xDF; //enable col 5 with 0, disable others with 1's
-	asm("nop"); //add a delay to allow PORTC to stabilize before checking
-	if(GetBit(PINC, 0) == 0) {return('2');}	
-	if(GetBit(PINC, 1) == 0) {return('5');}
-	if(GetBit(PINC, 3) == 0) {return('8');}
-	if(GetBit(PINC, 4) == 0) {return('0');}
+int keypadTick(int keypad_state){
+	unsigned char x = GetKeypadKey();
 	
-	//checks keys in col 3
-	PORTC = 0xBF; //enable col 6 with 0, disables other's with 1's
-	asm("nop");
-	if(GetBit(PINC, 0) == 0) {return('3');}
-	if(GetBit(PINC, 1) == 0) {return('6');}
-	if(GetBit(PINC, 2) == 0) {return('9');}
-	if(GetBit(PINC, 3) == 0) {return('#');}
-	
-	//checks keys in col 4
-	PORTC = 0x8F;
-	asm("nop");
-	if(GetBit(PINC, 0) == 0) {return('A');}
-	if(GetBit(PINC, 1) == 0) {return('B');}
-	if(GetBit(PINC, 2) == 0) {return('C');}
-	if(GetBit(PINC, 3) == 0) {return('D');}
+	switch(keypadState){
+		case start:
+			keypadState = start;
+			break;
+		
+		default:
+			break;
 
-	return('\0'); //default value
+	}
 
-}*/
+	switch(x){
+		case '\0':PORTB = 0X1F; break;
+		case '1': PORTB = 0x01; break;
+		case '2': PORTB = 0x02; break;
+		case '3': PORTB = 0x03; break;
+		case '4': PORTB = 0x04; break;
+		case '5': PORTB = 0x05; break;
+		case '6': PORTB = 0x06; break;
+		case '7': PORTB = 0x07; break;
+		case '8': PORTB = 0x08; break;
+		case '9': PORTB = 0X09; break;
+		case 'A': PORTB = 0x0A; break;
+		case 'B': PORTB = 0x0B; break;
+		case 'C': PORTB = 0x0C; break;
+		case 'D': PORTB = 0x0D; break;
+		case '*': PORTB = 0x0E; break;
+		case '#': PORTB = 0x0F; break;
+		default: PORTB = 0x1B; break;
 
+	}
+
+}
 
 int main(void) {
 	unsigned char x;
@@ -59,29 +61,36 @@ int main(void) {
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRA = 0xF0; PORTA = 0x0F;
     /* Insert your solution below */
-    while (1) {
-	x = GetKeypadKey();
 
-	switch(x){
-		case '\0': PORTB = 0x1F; break; //all 5 leds on
-		case '1': PORTB = 0x01; break; //hex equivalent
-		case '2': PORTB = 0x02; break;
-		case '3': PORTB = 0x03; break;
-		case '4': PORTB = 0x04; break;
-		case '5': PORTB = 0x05; break;
-		case '6': PORTB = 0x06; break;
-		case '7': PORTB = 0x07; break;
-		case '8': PORTB = 0x08; break;
-		case '9': PORTB = 0x09; break;
-		case '0': PORTB = 0x00; break;
-		case 'A': PORTB = 0x0A; break;
-		case 'B': PORTB = 0x0B; break;
-		case 'C': PORTB = 0x0C; break;
-		case 'D': PORTB = 0x0D; break;
-		case '*': PORTB = 0x0E; break;
-		case '#': PORTB = 0x0F; break;
-		default: PORTB = 0x1B; break; //should nevewr occur, middle led off
-	}
+	static task task1;
+	task *task[] = {&task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	task1.state = start;
+	task1.period = 50;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &keypadTick;
+
+	TimerSet(50);
+	TimerOn();
+
+
+    while (1) {
+
+	    for(unsigned short i = 0; i<numTasks; i++){
+		    if(tawsks[i]->elapsedTime == tasks[i]-> period){
+			    tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+			    tasks[i]->elapsedTime = 0;
+		    }
+
+		    tasks[i]->elapsedTime += 50;
+	    }
+
+	    keypadTick(start);
+	    while(!TimerFlag);
+	    TimerFlag = 0;
+
     }
+
     return 1;
 }
